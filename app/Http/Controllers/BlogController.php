423b122637bf53fe8376edf7219530479f40b965
diepz <you@example.cm>
2018-11-30 10:17:02 +0700

    done

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    public function index()
+    {
+        return Blog::all();
+    }
+
+    public function show($id)
+    {
+        return Blog::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $blog = Blog::create($request->all());
+        return response()->json($blog, 201);
+    }
+
+    public function update(Request $request, Blog $blog)
+    {
+        $blog->update($request->all());
+        return response()->json($blog, 200);
+    }
+
+    public function delete(Request $blog)
+    {
+        $blog->delete();
+
+        return response()->json($blog, 204);
+    }
+}
